5b51f44beaf5dc978da4c38f068389c9ac76a114
Joerg Planner <j.planner@gmx.de>
2018-10-19 15:58:49 +0200

    retitler, probably final

--- retitler.sh
+++ retitler.sh
@@ -12,28 +12,21 @@ fi
 
 for file in $( find $1 -name *.pdf )
 do
-  prefix=${file%/*}
-  old_base=${file##*/}
-  new_base=$( pdfinfo $file | grep Title | sed 's/Title:[[:space:]]*//' | sed 's/[[:space:]]/_/g' )
-  new_base=${new_base%.pdf}
+  prefix=${file%/*} #gets the prefix
+  old_base=${file##*/} #gets the suffix
+  new_base=$( pdfinfo $file | grep Title | sed 's/Title:[[:space:]]*//' | sed 's/[[:space:]]/_/g' ) #obtains metadata from pdf
+  new_base=${new_base%.pdf} #remove .pdf suffix if in metadatas title
   new_name=$prefix/$new_base.pdf
-  echo $prefix
-  echo $old_base
-  echo $new_base
-  echo $file
-  echo $new_name
-  echo 
   if [ -e $new_name ]
   then
-    echo skipping $file, because $new_name already exists!
+    echo WARNING: FILE ALREADY EXISTS: skipping $file, because $new_name already exists!
     continue
   elif [ -z $new_base ]
   then
-    echo skipping $file, because there was no Title metadata!
+    echo WARNING: NO TITLE: skipping $file, because there was no Title metadata!
     continue
   else
     echo renaming $file to $new_name
     mv $file $new_name
   fi
-  
 done
