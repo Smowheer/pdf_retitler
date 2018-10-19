ba7864893dff2fe9af979e99bb7d0de30ae47279
Joerg Planner <j.planner@gmx.de>
2018-10-19 15:50:22 +0200

    pdfinfo script

--- pdfinfos.sh
+++ pdfinfos.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+if [ $# != 1 ] || [ ! -d $1 ]; then
+  echo 'Usage: ./pdfinfos.sh <directory>'
+	exit
+fi
+
 for i in $( find $1 -name '*.pdf' ); do
   echo found $i;
   echo ----------;
-  pdfinfo $i;
+  pdfinfo $i | egrep "Title|Subject|Author|Pages|CreationDate";
   echo ----------;
 done
